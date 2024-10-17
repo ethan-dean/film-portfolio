8a503d3f418506765026758f3ea9117e17c94164
Ethan Dean <ethandean7@gmail.com>
2024-10-16 21:15:49 -0400

    Redesign to be less professional, add writing

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -8,8 +8,19 @@ function About() {
         <div className="about-text-container">
           <p className="about-text-title">Ally Farace</p>
           <span className="about-text-line"></span>
-          <p className="about-text-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quo possimus eaque, aspernatur asperiores accusamus doloribus odio dicta numquam. Quas inventore dignissimos dolore a nihil quo dolor voluptatibus animi velit.Fugiat quo possimus eaque, aspernatur asperiores accusamus doloribus odio dicta numquam.</p>
-          <p className="about-text-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quo possimus eaque, aspernatur asperiores accusamus doloribus odio dicta numquam. Quas inventore dignissimos dolore a nihil quo dolor voluptatibus animi velit.Fugiat quo possimus eaque, aspernatur asperiores accusamus doloribus odio dicta numquam.</p>
+          <p className="about-text-body">
+            Ally is the most beautiful and intelligent girl in the world.
+            Everytime I look at her I can't help but smile, happy that she is in my life.
+            She always knows how to solve my problems and saves me from stressing over things I don't need to.
+            When I am with her I feel safe and confident in myself.
+          </p>
+          <p className="about-text-body">
+            Ally is also incredibly sweet.
+            She never fails to warm my heart.
+            Anytime I see her smile it always makes my day brighter.
+            Whether it’s a kind word or a small gesture, she knows how to make me feel special and cared for.
+            Being with her makes me the luckiest person in the world.
+          </p>
         </div>
         <div className="about-image-container">
           <img className="about-image-1" src="/ally-1.png" />
@@ -21,8 +32,8 @@ function About() {
           <img className="about-image-3" src="/ally-3.png" />
         </div>
         <div className="about-text-container">
-          <p className="about-text-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quo possimus eaque, aspernatur asperiores accusamus doloribus odio dicta numquam. Quas inventore dignissimos dolore a nihil quo dolor voluptatibus animi velit.</p>
-          <p className="about-text-body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat quo possimus eaque, aspernatur asperiores accusamus doloribus odio dicta numquam. Quas inventore dignissimos dolore a nihil quo dolor voluptatibus animi velit.</p>
+          <p className="about-text-body">"Doubt thou the stars are fire, doubt that the sun doth move, but never doubt that Ally is a wonder, unmatched and unrivaled. She is my muse, my love, my everything."</p>
+          <p className="about-text-body">- Shakespeare... I think</p>
         </div>
       </div>
     </div>
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -10,13 +10,13 @@ function Navbar(props: any) {
           <img className="navbar-icon" src="/about-icon-black.png" />
         </button>
         <a className="navbar-link" href="https://www.imdb.com/name/nm15000024/" target="blank_" >
-          <p className="navbar-title" >Credits&nbsp;</p>
+          <p className="navbar-title" >Moviestar&nbsp;</p>
           <img className="navbar-img" src="/imdb-icon-black.png" />
         </a>
-        <a className="navbar-link" href="https://www.linkedin.com/in/ally-farace-b56941262/" target="blank_">
+        {/* <a className="navbar-link" href="https://www.linkedin.com/in/ally-farace-b56941262/" target="blank_">
           <p className="navbar-title" >Connect&nbsp;</p>
           <img className="navbar-img" src="/linkedin-icon-black.png" />
-        </a>
+        </a> */}
         <button className="navbar-button" onClick={() => props.handleClick("Cats")}>
           <p className="navbar-title" >Cats&nbsp;</p>
           <img className="navbar-icon" src="/paw-icon-black.png" />
